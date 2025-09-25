5a0080fb1aae1db5d474913e90a3a6795f5dbc0c
wh0smarg0 <wh0smarg0.s@gmail.com>
2025-09-25 12:56:26 +0300

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -44,30 +44,31 @@ export default function About() {
 
         <div className="profile-card">
           <h2>Сахно Маргарита Юріївна</h2>
-          <p>Placeholder</p>
-          <p>Placeholder</p>
+          <p>Народилася 11 липня 2006 року в Києві, Україна.</p>
+          <p>Закінчила Технічний ліцей НТУУ "КПІ", зараз навчаюсь 3 курс 123 спеціальність в КПІ на ФІОТ</p>
 
           <h3>Мої хобі:</h3>
           <ul>
-            <li>Placeholder</li>
-            <li>Placeholder</li>
-            <li>Placeholder</li>
+            <li>Гра на барабанах</li>
+            <li>Танці</li>
+            <li>LEGO</li>
           </ul>
 
           <h3>Улюблені книги/фільми:</h3>
           <ol>
-            <li>Placeholder</li>
-            <li>Placeholder</li>
-            <li>Placeholder</li>
+            <li>"A Good Girl's Guide to Murder" - Holly Jackson</li>
+            <li>"HTYYD - How to Train Your Dragon" - DreamWorks Animation</li>
+            <li>"The Hunger Games" - Suzanne Collins</li>
           </ol>
 
           <p>
-            Моє улюблене місто — Placeholder.
+            Моє улюблене місто — Вінниця. Вона подібна до Києва, але менша за розміром, більш спокійна, 
+            із своєрідною архітектурою та атмосферою, тому теж гарна і затишна.
           </p>
-          <a href="https://the-city.kiev.ua" target="_blank" rel="noopener noreferrer">
+          <a href="https://vinnytsia.city" target="_blank" rel="noopener noreferrer">
             <img
-              src="https://royaldesign.ua/file/495/cc/t9/Rx/1.BNb2.jpg"
-              alt="Батьківщина Мати, Київ"
+              src="https://back.uniqa.ua/storage/news/images/medium/vT6Vs2K3qyeaJBNCcRCbR8Z9BfFb8Jx58afS3clV.jpg?v=1722511676"
+              alt="Вулиці Вінниці"
               className="city-photo"
             />
           </a>
